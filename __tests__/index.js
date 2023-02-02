bfacd854cc63184c2c17c764b20cd14215eb6275
David Liu <david-khala@hotmail.com>
2023-02-02 22:27:28 +0800

    Don't use pure js for test

--- __tests__/index.js
+++
@@ -1,5 +0,0 @@
-describe(':for pure js', function () {
-	it('', async () => {
-
-	});
-});
---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/davidkhala/ts-collection.git .
+rm -rf .github .gitignore install.sh
+npm i
+npm test
+
+
